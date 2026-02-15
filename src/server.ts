806e7de52216ebf9737cf7cfae8cd25733ecb505
Kayron Cassamo <kayronlourencocassamo@gmail.com>
2026-02-15 23:51:32 +0200

    Configurando porta para render

--- src/server.ts
+++ src/server.ts
@@ -32,8 +32,8 @@ app.use((err:Error, req:Request, res:Response, next:NextFunction )=>
   }
 })
 
-app.listen(3333 ,() =>
+app.listen(process.env.PORT ,() =>
 {
-  console.log ('Servidor Online!!')
+  console.log ('Servidor Online!!', process.env.PORT)
 }
 )
